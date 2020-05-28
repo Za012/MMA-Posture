4488bf8df143f1cac7dc0c582c78073a615965f3
Liestere <550178@student.inholland.nl>
2020-05-28 21:15:32 +0200

    Added select directory and loading in files inside it + showing the image

--- fileLabeler.py
+++ fileLabeler.py
@@ -1,19 +1,31 @@
 # This Python file uses the following encoding: utf-8
 from PySide2 import QtCore, QtGui, QtWidgets
+from PySide2.QtWidgets import QFileDialog
+import os
 
 class FileLabeler:
     def __init__(self, ui):
         self.ui = ui
-        print("file labeler init")
-
-        frames = ["Frame1", "Frame2", "Frame3", "Frame4"]
-        self.ui.fileList.addItems(frames)
+        self.filelistpaths = []
 
     def attach(self):
         self.ui.fileList.itemClicked.connect(self.item_click)
+        self.ui.btnSelectDirectory.clicked.connect(self.directoryButtonClicked)
 
     def item_click(self, item):
-        self.ui.lblSelectedFile.setText(str(item.text()))
-        pixmap = QtGui.QPixmap(str(item.text())+'.png')
-        self.ui.imagePreview.setPixmap(pixmap)
-        self.ui.imagePreview.show()
+        for path in self.filelistpaths:
+            splitpath = path.split('/')
+            if(splitpath[len(splitpath)-1] == item.text()):
+                self.ui.lblSelectedFile.setText(str(item.text()))
+                pixmap = QtGui.QPixmap(path)
+                self.ui.imagePreview.setPixmap(pixmap)
+                self.ui.imagePreview.show()
+
+
+    def directoryButtonClicked(self):
+        dialog = QFileDialog.getExistingDirectory()
+        self.ui.fileList.addItems(os.listdir(dialog))
+        for file in os.listdir(dialog):
+            self.filelistpaths.append(dialog+"/"+file)
+        splitdirpath = dialog.split("/")
+        self.ui.lblSelectedDirectory.setText(splitdirpath[len(splitdirpath)-1])
