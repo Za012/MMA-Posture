01f96b54f4734e45522e8000eeb5912b42b11180
Zeid Adabel <zedjam98@hotmail.com>
2020-05-29 01:53:23 +0200

    Completely working according to architecture, Requires openpose to be setup using anaconda python37

--- ui_mainwindow.py
+++ ui_mainwindow.py
@@ -3,7 +3,7 @@
 ################################################################################
 ## Form generated from reading UI file 'mainwindow.ui'
 ##
-## Created by: Qt User Interface Compiler version 5.14.2
+## Created by: Qt User Interface Compiler version 5.15.0
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
@@ -31,26 +31,42 @@ class Ui_MainWindow(object):
         self.openpose.setObjectName(u"openpose")
         self.import_button = QPushButton(self.openpose)
         self.import_button.setObjectName(u"import_button")
-        self.import_button.setGeometry(QRect(400, 110, 75, 23))
+        self.import_button.setGeometry(QRect(381, 110, 120, 41))
+        font = QFont()
+        font.setPointSize(10)
+        self.import_button.setFont(font)
         self.process_button = QPushButton(self.openpose)
         self.process_button.setObjectName(u"process_button")
-        self.process_button.setGeometry(QRect(390, 220, 101, 40))
+        self.process_button.setGeometry(QRect(390, 260, 101, 40))
+        self.process_button.setFont(font)
         self.openpose_progress = QProgressBar(self.openpose)
         self.openpose_progress.setObjectName(u"openpose_progress")
-        self.openpose_progress.setGeometry(QRect(280, 270, 331, 23))
+        self.openpose_progress.setGeometry(QRect(280, 310, 331, 23))
         self.openpose_progress.setValue(24)
-        self.import_progress = QProgressBar(self.openpose)
-        self.import_progress.setObjectName(u"import_progress")
-        self.import_progress.setGeometry(QRect(280, 140, 331, 23))
-        self.import_progress.setValue(24)
         self.status_label = QLabel(self.openpose)
         self.status_label.setObjectName(u"status_label")
-        self.status_label.setGeometry(QRect(410, 380, 51, 16))
+        self.status_label.setGeometry(QRect(206, 390, 471, 51))
+        font1 = QFont()
+        font1.setPointSize(13)
+        self.status_label.setFont(font1)
         self.status_label.setAlignment(Qt.AlignCenter)
         self.filepaths_list = QListWidget(self.openpose)
         QListWidgetItem(self.filepaths_list)
         self.filepaths_list.setObjectName(u"filepaths_list")
         self.filepaths_list.setGeometry(QRect(60, 190, 151, 192))
+        self.clear_button = QPushButton(self.openpose)
+        self.clear_button.setObjectName(u"clear_button")
+        self.clear_button.setGeometry(QRect(60, 150, 151, 31))
+        self.clear_button.setFont(font)
+        self.batch_name = QTextEdit(self.openpose)
+        self.batch_name.setObjectName(u"batch_name")
+        self.batch_name.setGeometry(QRect(370, 190, 141, 31))
+        self.batch_name.setFont(font)
+        self.batch_name_label = QLabel(self.openpose)
+        self.batch_name_label.setObjectName(u"batch_name_label")
+        self.batch_name_label.setGeometry(QRect(380, 168, 121, 20))
+        self.batch_name_label.setFont(font)
+        self.batch_name_label.setAlignment(Qt.AlignCenter)
         self.tabWidget.addTab(self.openpose, "")
         self.labeler = QWidget()
         self.labeler.setObjectName(u"labeler")
@@ -93,6 +109,8 @@ class Ui_MainWindow(object):
         ___qlistwidgetitem.setText(QCoreApplication.translate("MainWindow", u"path/fileName", None));
         self.filepaths_list.setSortingEnabled(__sortingEnabled)
 
+        self.clear_button.setText(QCoreApplication.translate("MainWindow", u"Clear List", None))
+        self.batch_name_label.setText(QCoreApplication.translate("MainWindow", u"Enter Batch Name", None))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.openpose), QCoreApplication.translate("MainWindow", u"Keypoint Generator", None))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.labeler), QCoreApplication.translate("MainWindow", u"Labeling", None))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.teach), QCoreApplication.translate("MainWindow", u"Teach", None))
