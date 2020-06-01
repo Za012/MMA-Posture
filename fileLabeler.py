7d397211234a9a03c4d8164abe83624373487065
Liestere <550178@student.inholland.nl>
2020-06-01 12:32:40 +0200

    Added labeling for guard(green) and jab(red). Labled frames get added to labeledFrames array.

--- fileLabeler.py
+++ fileLabeler.py
@@ -1,5 +1,6 @@
 # This Python file uses the following encoding: utf-8
-from PySide2 import QtCore, QtGui, QtWidgets
+from PySide2 import QtGui
+from PySide2.QtGui import QBrush, QColor
 from PySide2.QtWidgets import QFileDialog
 import os
 
@@ -7,20 +8,39 @@ class FileLabeler:
     def __init__(self, ui):
         self.ui = ui
         self.filelistpaths = []
+        self.selectedPaths = []
+        self.labeledFrames = []
 
     def attach(self):
-        self.ui.fileList.itemClicked.connect(self.item_click)
+        self.ui.fileList.currentItemChanged.connect(self.itemSelectionChanged)
         self.ui.btnSelectDirectory.clicked.connect(self.directoryButtonClicked)
+        self.ui.btnGuard.clicked.connect(self.guardButtonClicked)
+        self.ui.btnJab.clicked.connect(self.jabButtonClicked)
 
-    def item_click(self, item):
+
+    def guardButtonClicked(self):
+        self.addLabeledFramesToArray("guard", QColor(105, 155, 103, 127))
+
+    def jabButtonClicked(self):
+        self.addLabeledFramesToArray("jab", QColor(255, 0, 0, 127))
+
+    def addLabeledFramesToArray(self, pose, color):
+        for item in self.ui.fileList.selectedItems():
+            for path in self.filelistpaths:
+                splitpath = path.split('/')
+                if splitpath[len(splitpath) - 1] == item.text():
+                    self.labeledFrames.append([pose, path])
+            item.setBackground(color)
+            item.setSelected(False)
+
+    def itemSelectionChanged(self, item):
         for path in self.filelistpaths:
             splitpath = path.split('/')
-            if(splitpath[len(splitpath)-1] == item.text()):
+            if splitpath[len(splitpath) - 1] == item.text():
                 self.ui.lblSelectedFile.setText(str(item.text()))
                 pixmap = QtGui.QPixmap(path)
                 self.ui.imagePreview.setPixmap(pixmap)
                 self.ui.imagePreview.show()
-
 
     def directoryButtonClicked(self):
         dialog = QFileDialog.getExistingDirectory()
